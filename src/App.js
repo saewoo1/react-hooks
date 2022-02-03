7d59050d94504706c84952ae18b87d4af6e42948
saewoo <wooozooo1123@gmail.com>
2022-02-03 15:48:37 +0900

    useMemo last

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-import React, { useState, useMemo } from 'react';
-
-const hardCalculate = (number) => {
-  console.log('계산이유');
-
-  for (let i = 0; i < 999999999; i++) {}
-  return number + 10000;
-}
-
-const easyCalculate = (number) => {
-  console.log('쉽다 쉬워');
-
-  return number + 1;
-}
+import React, { useState, useMemo, useEffect } from 'react';
 
 function App() {
-  const [hardNumber, setHardNumber] = useState(1);
-  const [easyNumber, setEasyNumber] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
- // const hardSum = hardCalculate(hardNumber);
-  const hardSum = useMemo(() => {
-    return hardCalculate(hardNumber)
-  }, [hardNumber]);
-  const easySum = easyCalculate(easyNumber);
+  // const location = {
+  //   country: isKorea ? '한국' : '외국',
+  // }; // re render를 하면 다른 메모리 상의 공간에 할당된거다!!! 새로운 주소의 obj임!!
+
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? '한국' : '외국',
+    }
+  }, [isKorea]); // 이제 이렇게 하면 number는 초기화에 관여하지 않는다!!
+  
+  useEffect (() => {
+    console.log("useEffect 호출")
+  }, [location]);
 
   return (
     <div>
-      <h3>어려븐 계산기</h3>
-      <input
+      <h2>하루에 몇끼 먹어요?</h2>
+      <input 
         type="number"
-        value={hardNumber}
-        onChange={(e) => setHardNumber(parseInt(e.target.value))}
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
       />
-      <span> + 10000 = {hardSum}</span>
 
-      <h3>쉬운 계산기</h3>
-      <input
-        type="number"
-        value={easyNumber}
-        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
-      />
-      <span> + 1 = {easySum}</span>
+      <hr />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라: {location.country}</p>
+      <button onClick={() => setIsKorea(!isKorea)}> 비행기 타자 </button>
     </div>
   );
 }
-
-//함수형 컴포넌트라서 .. easyNum이 바뀌어도 렌더링이 일어나고, hardNum도 호출하게 된다.
-//이제 useMemo를 사용하면 easyCal의 값이 바뀌었을 땐 빨리 빨리 잘 된다! -> 어려운 계산 실행 X
 
 export default App;
