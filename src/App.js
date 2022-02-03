1bcb2496c8e48439b8b8e30b3aee8d7a3936c0d7
saewoo <wooozooo1123@gmail.com>
2022-02-03 10:07:49 +0900

    useMemo - easysum

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ const hardCalculate = (number) => {
   return number + 10000;
 }
 
+const easyCalculate = (number) => {
+  console.log('쉽다 쉬워');
+
+  return number + 1;
+}
+
 function App() {
   const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
 
   const hardSum = hardCalculate(hardNumber);
+  const easySum = easyCalculate(easyNumber);
 
   return (
     <div>
@@ -21,6 +29,14 @@ function App() {
         onChange={(e) => setHardNumber(parseInt(e.target.value))}
       />
       <span> + 10000 = {hardSum}</span>
+
+      <h3>쉬운 계산기</h3>
+      <input
+        type="number"
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span> + 1 = {easySum}</span>
     </div>
   );
 }
